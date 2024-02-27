b4ecc6f03bc4baa2dec298315083f949b6ab7316
Jean <771058475@qq.com>
2024-02-27 08:34:07 +0800

    202402270834

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -3,29 +3,29 @@
         <button>原生按钮</button>
         <input type="text">
         <hr>
-        <el-row>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-        </el-row>
+        <jean-row>
+            <jean-button>默认按钮</jean-button>
+            <jean-button type="primary">主要按钮</jean-button>
+            <jean-button type="success">成功按钮</jean-button>
+            <jean-button type="info">信息按钮</jean-button>
+            <jean-button type="warning">警告按钮</jean-button>
+            <jean-button type="danger">危险按钮</jean-button>
+        </jean-row>
         <hr>
-        <el-row>
-            <el-button icon="el-icon-search" circle></el-button>
-            <el-button type="primary" icon="el-icon-edit" circle></el-button>
-            <el-button type="success" icon="el-icon-check" circle></el-button>
-            <el-button type="info" icon="el-icon-message" circle></el-button>
-            <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-            <el-button type="danger" icon="el-icon-delete" circle></el-button>
-        </el-row>
+        <jean-row>
+            <jean-button icon="el-icon-search" circle></jean-button>
+            <jean-button type="primary" icon="el-icon-edit" circle></jean-button>
+            <jean-button type="success" icon="el-icon-check" circle></jean-button>
+            <jean-button type="info" icon="el-icon-message" circle></jean-button>
+            <jean-button type="warning" icon="el-icon-star-off" circle></jean-button>
+            <jean-button type="danger" icon="el-icon-delete" circle></jean-button>
+        </jean-row>
         <hr>
-        <el-date-picker
+        <jean-date-picker
             v-model="value1"
             type="date"
             placeholder="选择日期">
-        </el-date-picker>
+        </jean-date-picker>
     </div>
 </template>
 <script>
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -4,11 +4,18 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //引入ElementUI
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
 //引入element-ui样式
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI)
+//引入部分ui组件(按需引入)
+import { Button,Row,DatePicker } from 'element-ui'
+
+// Vue.use(ElementUI)
+
+Vue.component('jean-row',Row)
+Vue.component('jean-button',Button)
+Vue.component('jean-date-picker',DatePicker)
 
 //关闭vue的生产提示
 Vue.config.productionTip = false
